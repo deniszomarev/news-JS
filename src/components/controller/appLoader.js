e228d73ad2375ded173bfdc0ab57c2210a7ffbbc
Denis Zomarev <eek@live.ru>
2022-08-11 16:05:49 +0300

    Loader api key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e03d2620a26e4912ad4209f03b676364', // получите свой ключ https://newsapi.org/
         });
     }
 }
